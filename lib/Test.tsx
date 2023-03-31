14fe80005eaefb221e68a77c94d86edf8593d5f7
김재현 <kimreplay@icraft21.com>
2023-03-31 15:29:32 +0900

    라이브러리화 태스트

---
+++ lib/Test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ITestProps {
+    //children?: React.ReactNode,
+}
+
+const Test = (
+    {}: ITestProps
+) => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Test;
