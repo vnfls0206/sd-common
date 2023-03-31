320c8a2c862224e820b539eb6a7d7a299d7bdf34
김재현 <kimreplay@icraft21.com>
2023-03-31 17:55:36 +0900

    lib 업데이트

---
+++ lib/five.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface IFiveProps {
+    //children?: React.ReactNode,
+}
+
+const Five = (
+    {}: IFiveProps
+) => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Five;
