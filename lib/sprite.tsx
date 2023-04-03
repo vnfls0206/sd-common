7be57bc3f3c99518d1440de75b70253acb594074
김재현 <vnlfs0206@gmail.com>
2023-04-03 13:30:23 +0900

    lib

---
+++ lib/sprite.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ISpriteProps {
+    //children?: React.ReactNode,
+}
+
+const Sprite = (
+    {}: ISpriteProps
+) => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Sprite;
