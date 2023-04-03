f0f929876f8ec48676f29f8a35109f836c9b4096
김재현 <vnlfs0206@gmail.com>
2023-04-03 20:40:23 +0900

    lib

--- src/ui/atom/PageTitle.tsx
+++ src/ui/atom/PageTitle.tsx
@@ -13,7 +13,7 @@ const PageTitle = (
 
     return (
         <React.Fragment>
-            <div>{'dinner overover'}</div>
+            <div>{'dinner overoverover'}</div>
             <h2 className={'text-2xl opacity-80 mt-7 mb-5'}>
                 {pageTitle}
             </h2>
