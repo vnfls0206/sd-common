02dfe09089340cb0be047e778aca8c512672b572
김재현 <kimreplay@icraft21.com>
2023-04-03 11:18:59 +0900

    lib

---
+++ lib/tsm.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ITsmProps {
+    //children?: React.ReactNode,
+}
+
+const Tsm = (
+    {}: ITsmProps
+) => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Tsm;
