de5cee1c163b65476ea296bf0dbd78a7f8c31692
김재현 <kimreplay@icraft21.com>
2023-04-03 10:37:04 +0900

    lib 11

---
+++ lib/lll.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ILllProps {
+    //children?: React.ReactNode,
+}
+
+const Lll = (
+    {}: ILllProps
+) => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Lll;
