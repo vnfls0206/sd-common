e6998434313b337ee403074c3b1a5a2984598163
김재현 <vnlfs0206@gmail.com>
2023-04-03 20:28:57 +0900

    lib

--- src/ui/atom/PageTitle.tsx
+++ src/ui/atom/PageTitle.tsx
@@ -13,7 +13,7 @@ const PageTitle = (
 
     return (
         <React.Fragment>
-            <div>{'dinner time'}</div>
+            <div>{'dinner over'}</div>
             <h2 className={'text-2xl opacity-80 mt-7 mb-5'}>
                 {pageTitle}
             </h2>
