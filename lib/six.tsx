6dab0bdfbb742c53fc1d28b0e7813de001e32457
김재현 <kimreplay@icraft21.com>
2023-04-03 10:02:35 +0900

    lib 업데이트

---
+++ lib/six.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ISixProps {
+    //children?: React.ReactNode,
+}
+
+const Six = (
+    {}: ISixProps
+) => {
+
+    return (
+        <div>
+            </div>
+    );
+};
+
+export default Six;
