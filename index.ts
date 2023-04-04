11f4e9d48861fdb2eb3766501953e7790a004c56
김재현 <vnlfs0206@gmail.com>
2023-04-04 20:57:04 +0900

    lib

--- index.ts
+++ index.ts
@@ -2,4 +2,3 @@
 export * from './src/ui';
 
 export {PageTitle} from './src/ui';
-export {default} from "./src/ui";
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,3 @@
-export {default as PageTitle } from './atom/PageTitle';
+import PageTitle from "./atom/PageTitle";
 
-
-
-declare const _default: 'Atom';
-export default _default;
+export {PageTitle}
