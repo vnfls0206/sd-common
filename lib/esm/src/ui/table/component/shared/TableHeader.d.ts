025e90e63f9361a5966fb816c3637d7217405bb9
김재현 <vnlfs0206@gmail.com>
2023-04-05 20:12:44 +0900

    lib

--- lib/esm/src/ui/table/component/shared/TableHeader.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export interface ITableHeaderProps {
-    size: string;
-    children?: React.ReactNode;
-}
-declare const TableHeader: ({ size, children }: ITableHeaderProps) => JSX.Element;
-export default TableHeader;
