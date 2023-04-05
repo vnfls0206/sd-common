2ced6383798695a8a10390b8344d27c361946abb
김재현 <vnlfs0206@gmail.com>
2023-04-05 18:14:08 +0900

    lib

---
+++ lib/esm/src/ui/table/index.d.ts
@@ -0,0 +1,5 @@
+export { default as useTable } from './useTable';
+export { default as Table } from './useTable';
+export { default as TableService } from './service';
+export * from './datagrid/interface';
+export * from './muitable/interface';
--- src/ui/table/index.ts
+++ src/ui/table/index.ts
@@ -1,8 +1,14 @@
 export {default as useTable} from './useTable';
 
-export {default as Table} from './useTable';
+export {Table} from './Table';
 
 export {default as TableService} from './service';
 
 export * from './datagrid/interface';
+export {default as ReactDataGrid} from './datagrid/ReactDataGrid';
+
+
+export {default as CustomColumnTable} from './muitable/CustomColumnTable';
+export {default as CustomStyleTable} from './muitable/CustomStyleTable';
+export {default as RawClickMuiTable} from './muitable/RawClickMuiTable';
 export * from './muitable/interface';
