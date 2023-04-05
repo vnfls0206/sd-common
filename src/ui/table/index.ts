9972e3c9b498a659eb00a98f5882cf6487129bf4
김재현 <vnlfs0206@gmail.com>
2023-04-05 19:35:51 +0900

    lib

--- src/ui/table/index.ts
+++ src/ui/table/index.ts
@@ -5,10 +5,8 @@ export {Table} from './Table';
 export {default as TableService} from './service';
 
 export * from './datagrid/interface';
-export {default as ReactDataGrid} from './datagrid/ReactDataGrid';
-
-
-export {default as CustomColumnTable} from './muitable/CustomColumnTable';
-export {default as CustomStyleTable} from './muitable/CustomStyleTable';
-export {default as RawClickMuiTable} from './muitable/RawClickMuiTable';
+//export {default as ReactDataGrid} from './datagrid/ReactDataGrid';
+// export {default as CustomColumnTable} from './muitable/CustomColumnTable';
+// export {default as CustomStyleTable} from './muitable/CustomStyleTable';
+// export {default as RawClickMuiTable} from './muitable/RawClickMuiTable';
 export * from './muitable/interface';
