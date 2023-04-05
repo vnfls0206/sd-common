97d1c6e9ef7ea0ba7bac584c720a9234781d40ed
김재현 <vnlfs0206@gmail.com>
2023-04-05 19:49:46 +0900

    lib

--- lib/esm/src/ui/table/index.d.ts
+++ lib/esm/src/ui/table/index.d.ts
@@ -1,5 +1,2 @@
 export { default as useTable } from './useTable';
 export { Table } from './Table';
-export { default as TableService } from './service';
-export * from './datagrid/interface';
-export * from './muitable/interface';
