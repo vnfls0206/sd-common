12d1d20dbef855292464e5b46268fdeb7c63461a
김재현 <vnlfs0206@gmail.com>
2023-04-05 10:49:02 +0900

    lib

--- lib/src/ui/atom/PageTitle.d.ts
+++ lib/src/ui/atom/PageTitle.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-export interface IPageTitleProps {
+interface IPageTitleProps {
     pageTitle: string;
 }
 declare const PageTitle: ({ pageTitle, }: IPageTitleProps) => JSX.Element;
