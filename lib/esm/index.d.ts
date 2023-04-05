4a11d1051abf8c155bcf873acc287ed791daf84f
김재현 <vnlfs0206@gmail.com>
2023-04-05 19:15:29 +0900

    lib

--- lib/esm/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/ui/atom';
-export * from './src/ui/table';
--- lib/esm/src/ui/table/muitable/UI/molecules/SearchRow.d.ts
+++
@@ -1,7 +0,0 @@
-import { ISearchFormProps } from "../../../../../../UI/molecules/SearchForm";
-import { GridColDef } from "@mui/x-data-grid";
-export interface ISearchTableProps extends Omit<ISearchFormProps, 'array'> {
-    columns: GridColDef[];
-}
-declare const SearchRow: ({ columns, onChange, resetOnClick, register, }: ISearchTableProps) => import("../../interface").ICustomColumnRow[];
-export default SearchRow;
