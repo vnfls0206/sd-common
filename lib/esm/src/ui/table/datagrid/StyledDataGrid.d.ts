964f920c5fdfe445a9b5325917e4876ef06cf541
김재현 <vnlfs0206@gmail.com>
2023-04-06 13:33:42 +0900

    lib

--- lib/esm/src/ui/table/datagrid/StyledDataGrid.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { IStyledDataGridType } from './interface';
-declare const StyledDataGrid: {
-    ({ styledType, ...props }: IStyledDataGridType): JSX.Element;
-    defaultProps: {
-        styledType: "DEFAULT";
-    };
-};
-export default StyledDataGrid;
