aa688a52dc7e60d0e56419aac736198e03961cbf
김재현 <vnlfs0206@gmail.com>
2023-04-06 13:41:53 +0900

    lib

--- lib/src/ui/table/datagrid/styled/TestStyledDataGrid.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { DataGridProps } from '@mui/x-data-grid';
-declare const TestStyledDataGrid: (props: DataGridProps) => JSX.Element;
-export default TestStyledDataGrid;
