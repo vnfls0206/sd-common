295d271c94635d1c09f4e1f6cc95133c829ec744
김재현 <vnlfs0206@gmail.com>
2023-04-06 19:42:16 +0900

    lib

--- lib/atom/index.d.ts
+++ lib/atom/index.d.ts
@@ -1,7 +1,3 @@
-export {default as TestDiv} from './TestDiv';
-
-
-export {default as PageTitle} from './PageTitle';
-
-export {default as ModalInput} from './ModalInput';
-
+export { default as TestDiv } from './TestDiv';
+export { default as PageTitle } from './PageTitle';
+export { default as ModalInput } from './ModalInput';
