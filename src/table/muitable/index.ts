6abf5f6c624213932b6abc17e84b5ec1f267c063
김재현 <vnlfs0206@gmail.com>
2023-04-06 16:54:16 +0900

    lib

--- src/table/muitable/index.ts
+++ src/table/muitable/index.ts
@@ -2,6 +2,5 @@ export { default } from './CustomStyleTable';
 export { default } from './RawClickMuiTable';
 export { default } from './CustomColumnTable';
 
-export * from './ReactDataGrid';
 
 export * from './interface'
