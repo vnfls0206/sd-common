686830565a0810ef3ad0d62083240b8185958eaf
김재현 <vnlfs0206@gmail.com>
2023-04-06 15:06:44 +0900

    lib

--- lib/cjs/table/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { default as useTable } from './useTable';
-export { Table } from './Table';
-export { default as TableService } from './service';
-export * from './datagrid/interface';
-export { default as ReactDataGrid } from './datagrid/ReactDataGrid';
-export { default as CustomColumnTable } from './muitable/CustomColumnTable';
-export { default as CustomStyleTable } from './muitable/CustomStyleTable';
-export { default as RawClickMuiTable } from './muitable/RawClickMuiTable';
-export * from './muitable/interface';
