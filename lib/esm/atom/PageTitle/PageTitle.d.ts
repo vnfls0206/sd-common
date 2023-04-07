0f148b2b5348a99560e85849b5458f234cf5f7f9
김재현 <vnlfs0206@gmail.com>
2023-04-07 13:07:07 +0900

    lib

--- lib/esm/atom/PageTitle/PageTitle.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface IPageTitleProps {
-    pageTitle: string;
-}
-declare const PageTitle: ({ pageTitle, }: IPageTitleProps) => JSX.Element;
-export default PageTitle;
